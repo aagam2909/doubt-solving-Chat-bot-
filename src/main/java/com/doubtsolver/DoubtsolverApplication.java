490313d98dcad10a2594076c8ef58d0883ab261a
deepak.devdd.97@gmail.com <U@tusees@#1997>
2026-02-22 14:00:33 +0530

    initial commit

---
+++ src/main/java/com/doubtsolver/DoubtsolverApplication.java
@@ -0,0 +1,13 @@
+package com.doubtsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubtsolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubtsolverApplication.class, args);
+	}
+
+}
